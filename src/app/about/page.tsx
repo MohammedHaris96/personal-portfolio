13ef29415a8873ee2ac15d43e14e4412d84c0f0c
Haris <mhaaris0815@gmail.com>
2024-03-14 23:25:58 +0530

    routes and tailwind css setup done

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <p>About</p>
+    </main>
+  );
+}
+
+export default index;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <p>Contact</p>
+    </main>
+  );
+};
+
+export default index;
