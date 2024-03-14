81150c9bd691de9c52a70ca80da4617d7e755409
Haris <mhaaris0815@gmail.com>
2024-03-14 23:30:03 +0530

    fixed header display issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <Header/>
+      {/* <Header/> */}
       <main className="flex min-h-screen flex-col items-center justify-between p-10">
         <p>Landing Page</p>
       </main>
